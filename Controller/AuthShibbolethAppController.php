b3cc07014c1b27e3a3e6d99f08413845cb883047
Mitsuru Mutaguchi <akagane99@gmail.com>
2017-11-15 01:15:53 +0000

    add lang file

---
+++ Controller/AuthShibbolethAppController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * AuthShibbolethApp Controller
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('AuthController', 'Auth.Controller');
+
+/**
+ * 認証処理
+ * ※AuthControllerを継承する
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\AuthShibboleth\Controller
+ */
+class AuthShibbolethAppController extends AuthController {
+
+}
